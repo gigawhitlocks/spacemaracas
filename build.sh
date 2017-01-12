856fb6a0c13f99264de7c4ff396f0b561e8d28b0
Ian Whitlock <iwhitlock@vmware.com>
2017-01-12 14:35:28 -0600

    local config with noauth

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#/bin/bash
+#!/bin/bash
 
 echo "Building emacs container"
 docker build -t emacs .
+docker tag emacs gigawhitlocks/emacs:latest
 
 echo "Build custom guacamole"
 docker build -t guac ./guacamole
+docker tag guac gigawhitlocks/guac:latest
--- maracas.sh
+++ maracas.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
-echo "Building emacs container"
-docker build -t emacs .
+# echo "Building emacs container"
+# docker build -t emacs .
 
 echo "Running emacs container"
-docker run -d --name some-emacs -p 5900:5900 emacs
+docker run -d --name some-emacs -p 5900:5900 gigawhitlocks/emacs:latest
 
 echo "Running guacd"
-docker run --name some-guacd -d -p 4822:4822 --link some-emacs:emacs guacamole/guacd
+docker run --name some-guacd -d -p 4822:4822 --link some-emacs:emacs guacamole/guacd:latest
 
-echo "Generate SQL init script"
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > initdb.sql
+# echo "Generate SQL init script"
+# docker run guacamole/guacamole:latest /opt/guacamole/bin/initdb.sh --postgres > initdb.sql
 
-echo "Starting postgres"
-docker run --name guac-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-sleep 5
+# echo "Starting postgres"
+# docker run --name guac-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+# sleep 5
 
-echo "Create guacamole db"
-docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
-       psql -h postgres -U postgres -c "CREATE DATABASE guacamole_db;"
+# echo "Create guacamole db"
+# docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
+#        psql -h postgres -U postgres -c "CREATE DATABASE guacamole_db;"
 
-echo "Run SQL init script against the db"
-docker run -e PGPASSWORD=mysecretpassword -v $(pwd):/tmp --rm --link guac-postgres:postgres postgres \
-       psql -h postgres -U postgres -d "guacamole_db" -f /tmp/initdb.sql
+# echo "Run SQL init script against the db"
+# docker run -e PGPASSWORD=mysecretpassword -v $(pwd):/tmp --rm --link guac-postgres:postgres postgres \
+#        psql -h postgres -U postgres -d "guacamole_db" -f /tmp/initdb.sql
 
-echo "Create roles"
-docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
-       psql -h postgres -U postgres -d "guacamole_db" -c \
-       "CREATE USER guacamole_user WITH PASSWORD 'some_password';
-       GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;
-       GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"
+# echo "Create roles"
+# docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
+#        psql -h postgres -U postgres -d "guacamole_db" -c \
+#        "CREATE USER guacamole_user WITH PASSWORD 'some_password';
+#        GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;
+#        GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"
+
+# echo "Build custom guacamole"
+# docker build -t guac ./guacamole
 
 echo "Run guacamole"
-docker run --name some-guacamole --link some-guacd:guacd \
-       --link guac-postgres:postgres      \
+
+           # # --link guac-postgres:postgres      \
+           #     -e POSTGRES_DATABASE=guacamole_db  \
+           #        -e POSTGRES_USER=guacamole_user    \
+           #        -e POSTGRES_PASSWORD=some_password \
+
+docker run --name some-guacamole\
+       --link some-guacd:guacd \
        --link some-emacs:emacs \
-       -e POSTGRES_DATABASE=guacamole_db  \
-       -e POSTGRES_USER=guacamole_user    \
-       -e POSTGRES_PASSWORD=some_password \
-       -d -p 8080:8080 guacamole/guacamole:latest
+       -d -p 8080:8080 gigawhitlocks/guac:latest
 
 docker ps
-echo -e "####\nSuccess!\nOpen to localhost:8080 in a browser or connect a vnc viewer to localhost:5900"
+echo -e "\n\n####\nSuccess!\nOpen to localhost:8080 in a browser or connect a vnc viewer to localhost:5900"
