340491c26f670507993e327b4486cd68e0f0b8c2
Ian Whitlock <ian@theknown.net>
2017-01-13 15:19:39 -0600

    reorganization and unified helper script

---
+++ local/clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker rm -f some-emacs some-guacamole guac-postgres some-guacd
--- maracas.sh
+++ maracas.sh
@@ -1,52 +1,36 @@
 #!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
 
-# echo "Building emacs container"
-# docker build -t emacs .
+usage=$(cat <<EOF
+Spacemaracas - the beginnings of a vnc emacs thing with lazy documentation
 
-echo "Running emacs container"
-docker run -d --name some-emacs -p 5900:5900 gigawhitlocks/emacs:latest
+Usage:
+$0 run -- just run the thing. Will pull images if they are not built.
+$0 build -- Build images from source (more recent, less stable)
+$0 clean -- Delete the Docker containers left around after running
+$0 help -- Display this help screen
+EOF
+     )
 
-echo "Running guacd"
-docker run --name some-guacd -d -p 4822:4822 --link some-emacs:emacs guacamole/guacd:latest
 
-# echo "Generate SQL init script"
-# docker run guacamole/guacamole:latest /opt/guacamole/bin/initdb.sh --postgres > initdb.sql
+cd local
+if test $# -eq 0; then
+    echo "$usage"
+    exit 0
+fi
 
-# echo "Starting postgres"
-# docker run --name guac-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-# sleep 5
+while test $# -gt 0
+do
+    case "$1" in
+        build) ./build.sh
+                ;;
+        run) ./run.sh
+                ;;
+        clean) ./clean.sh
+             ;;
+        *) echo "$usage"
+           ;;
+    esac
+    shift
+done
 
-# echo "Create guacamole db"
-# docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
-#        psql -h postgres -U postgres -c "CREATE DATABASE guacamole_db;"
-
-# echo "Run SQL init script against the db"
-# docker run -e PGPASSWORD=mysecretpassword -v $(pwd):/tmp --rm --link guac-postgres:postgres postgres \
-#        psql -h postgres -U postgres -d "guacamole_db" -f /tmp/initdb.sql
-
-# echo "Create roles"
-# docker run -e PGPASSWORD=mysecretpassword --rm --link guac-postgres:postgres postgres\
-#        psql -h postgres -U postgres -d "guacamole_db" -c \
-#        "CREATE USER guacamole_user WITH PASSWORD 'some_password';
-#        GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;
-#        GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;"
-
-# echo "Build custom guacamole"
-# docker build -t guac ./guacamole
-
-echo "Run guacamole"
-
-           # # --link guac-postgres:postgres      \
-           #     -e POSTGRES_DATABASE=guacamole_db  \
-           #        -e POSTGRES_USER=guacamole_user    \
-           #        -e POSTGRES_PASSWORD=some_password \
-
-docker run --name some-guacamole\
-       --link some-guacd:guacd \
-       --link some-emacs:emacs \
-       -d -p 8080:8080 gigawhitlocks/guac:latest
-
-docker ps
-echo -e "\n\n####\nSuccess!\nOpen to localhost:8080 in a browser or connect a vnc viewer to localhost:5900"
+exit 0
