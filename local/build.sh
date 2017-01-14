6de48f3cb9506259de26a2e6ece2f1c2d82baf82
Ian Whitlock <ian@theknown.net>
2017-01-13 22:10:02 -0600

    getting ready for a multiplayer version

--- local/build.sh
+++ local/build.sh
@@ -4,6 +4,10 @@ echo "Building emacs container"
 docker build -t emacs .
 docker tag emacs gigawhitlocks/emacs:latest
 
-echo "Build custom guacamole"
-docker build -t guac ./guacamole
-docker tag guac gigawhitlocks/guac:latest
+echo "Build base for custom guacamoles"
+docker build -t guac ../guacamole
+docker tag guac gigawhitlocks/guacamole:latest
+
+echo "Build local guacamole"
+docker build -t local-guac ./guacamole
+docker tag local-guac gigawhitlocks/local-guac:latest
